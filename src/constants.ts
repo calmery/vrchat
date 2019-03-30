7b2c898a289526d374dbf9dbf771d4b5c530c708
Marei Kikukawa <contact@calmery.me>
2019-03-30 20:17:22 +0900

    dev-api と beta-api は使用しないように変更する

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,1 @@
-import { VRChatAPIBaseUrl } from "./types";
-
-export const VRCHAT_API_BASE_URL: VRChatAPIBaseUrl =
-  "https://api.vrchat.cloud/api/1/";
-export const VRCHAT_API_BASE_URL_DEVELOPMENT: VRChatAPIBaseUrl =
-  "https://dev-api.vrchat.cloud/api/1/";
-export const VRCHAT_API_BASE_URL_BETA: VRChatAPIBaseUrl =
-  "https://beta-api.vrchat.cloud/api/1/";
+export const VRCHAT_API_BASE_URL = "https://api.vrchat.cloud/api/1/";
--- src/urlBuilder.ts
+++ src/urlBuilder.ts
@@ -1,27 +1,8 @@
 import * as url from "url";
-import { VRChatAPIBaseUrl } from "./types";
-import {
-  VRCHAT_API_BASE_URL,
-  VRCHAT_API_BASE_URL_DEVELOPMENT,
-  VRCHAT_API_BASE_URL_BETA
-} from "./constants";
+import { VRCHAT_API_BASE_URL } from "./constants";
 
-export interface UrlBuilder {
-  config: () => string;
-}
-
-const generateUrlBuilder = (baseUrl: VRChatAPIBaseUrl): UrlBuilder => {
-  return {
-    config() {
-      return url.resolve(baseUrl, "config");
-    }
-  };
+export default {
+  config() {
+    return url.resolve(VRCHAT_API_BASE_URL, "config");
+  }
 };
-
-export const urlBuilder = generateUrlBuilder(VRCHAT_API_BASE_URL);
-export const urlBuilderUsingDevelopmentUrl = generateUrlBuilder(
-  VRCHAT_API_BASE_URL_DEVELOPMENT
-);
-export const urlBuilderUsingBetaUrl = generateUrlBuilder(
-  VRCHAT_API_BASE_URL_BETA
-);
