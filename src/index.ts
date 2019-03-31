51500c36247bd3fa8f363591d8521bc12b483b2b
Marei Kikukawa <contact@calmery.me>
2019-03-31 16:48:27 +0900

    API をクラス化してエクスポートする

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,50 @@
-import { getAPIKey } from "./api";
+import { AxiosInstance } from "axios";
+import { login, logout } from "./api";
+export { getAPIKey } from "./api";
 
-const main = async (): Promise<void> => {
-  const apiKey = await getAPIKey();
+export class VRChat {
+  private apiKey = "JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26"; // As of March 31, 2019.
+  private axiosInstance: AxiosInstance | null = null;
 
-  if (apiKey === null) {
-    return;
+  constructor(apiKey?: string) {
+    if (apiKey !== undefined) {
+      this.apiKey = apiKey;
+    }
   }
 
-  // eslint-disable-next-line no-console
-  console.log("API Key :", apiKey);
-};
+  async login(username: string, password: string) {
+    if (this.axiosInstance !== null) {
+      return true;
+    }
 
-main();
+    const axiosInstance = await login(this.apiKey, username, password);
+
+    if (axiosInstance === null) {
+      return false;
+    }
+
+    this.axiosInstance = axiosInstance;
+
+    return true;
+  }
+
+  async logout() {
+    if (this.axiosInstance === null) {
+      return true;
+    }
+
+    const logoutResult = await logout(this.axiosInstance);
+
+    if (!logoutResult) {
+      return false;
+    }
+
+    this.axiosInstance = null;
+
+    return true;
+  }
+
+  getAxiosInstance() {
+    return this.axiosInstance;
+  }
+}
