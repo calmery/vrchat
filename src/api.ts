f1b3f25befe5cee52cf6b83b912dfff45bef7041
Marei Kikukawa <contact@marei.jp>
2019-03-31 16:44:38 +0900

    ログイン，ログアウトの API を追加する

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,16 @@
-import { AxiosResponse } from "axios";
+import axios, { AxiosResponse, AxiosInstance } from "axios";
 import { VRChatConfig } from "./types";
 import * as requestBuilder from "./requestBuilder";
+import { VRCHAT_API_BASE_URL } from "./constants";
+
+const generateAxiosInstanceByCookie = (cookie: string[]) => {
+  return axios.create({
+    baseURL: VRCHAT_API_BASE_URL,
+    headers: {
+      Cookie: cookie
+    }
+  });
+};
 
 export const getConfig = async (): Promise<VRChatConfig | null> => {
   try {
@@ -9,5 +19,34 @@ export const getConfig = async (): Promise<VRChatConfig | null> => {
     return response.data;
   } catch {
     return null;
+  }
+};
+
+export const login = async (
+  apiKey: string,
+  username: string,
+  password: string
+): Promise<AxiosInstance | null> => {
+  try {
+    const { headers } = await requestBuilder.login(apiKey, username, password);
+
+    if (!headers.hasOwnProperty("set-cookie")) {
+      return null;
+    }
+
+    return generateAxiosInstanceByCookie(headers["set-cookie"]);
+  } catch {
+    // If the status code is 401, API key may be invalid.
+    return null;
+  }
+};
+
+export const logout = async (axiosInstance: AxiosInstance) => {
+  try {
+    await requestBuilder.logout(axiosInstance);
+
+    return true;
+  } catch {
+    return false;
   }
 };
--- src/requestBuilder.ts
+++ src/requestBuilder.ts
@@ -1,9 +1,24 @@
 import { resolve } from "url";
 import { VRCHAT_API_BASE_URL } from "./constants";
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 
 const generateRequestURL = (endpoint: string) => {
   return resolve(VRCHAT_API_BASE_URL, endpoint);
+};
+
+export const login = (apiKey: string, username: string, password: string) => {
+  const url = generateRequestURL("auth/user");
+
+  return axios.get(url, {
+    params: { apiKey },
+    auth: { username, password }
+  });
+};
+
+export const logout = (axiosInstance: AxiosInstance) => {
+  const url = generateRequestURL("logout");
+
+  return axiosInstance.put(url);
 };
 
 export const config = () => {
